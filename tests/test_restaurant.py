c5a7fcb94e080d50e35019c3eb9d8b0d97b3cf0e
Diego Cardoso <DiegoCardoso@microsoft.com>
2025-05-08 16:37:43 -0300

    sugestão de melhorias

--- tests/test_restaurant.py
+++ tests/test_restaurant.py
@@ -1,3 +1,4 @@
+import pytest
 from app.restaurant import RestaurantApp
 
 class TestRestaurantApp: 
@@ -23,7 +24,6 @@ class TestRestaurantApp:
         # assert
         assert currentValue == expectedValue
 
-
     def test_when_close_restaurant_then_status_closed(self):
         # arrange
         expectedValue = "closed"
@@ -35,134 +35,39 @@ class TestRestaurantApp:
         currentValue = app.status
         # assert
         assert currentValue == expectedValue
-        
-    def test_when_input_morning_then_output(self):
-        # arrange
-        input = "morning"
-        expectedOutput = ""
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_night_then_output(self):
-        # arrange
-        input = "night"
-        expectedOutput = ""
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_night_upper_then_output(self):
-        # arrange
-        input = "NIGHT"
-        expectedOutput = ""
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_morning123_then_output(self):
-        # arrange
-        input = "morning, 1, 2, 3"
-        expectedOutput = "eggs, toast, coffee"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-        
-    def test_when_input_morning213_then_output(self):
-        # arrange
-        input = "morning, 2, 1, 3"
-        expectedOutput = "eggs, toast, coffee"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_morning1234_then_output(self):
-        # arrange
-        input = "morning, 1, 2, 3, 4"
-        expectedOutput = "eggs, toast, coffee, error"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_morning12333_then_output(self):
-        # arrange
-        input = "morning, 1, 2, 3, 3, 3"
-        expectedOutput = "eggs, toast, coffee(x3)"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_night1234_then_output(self):
-        # arrange
-        input = "night, 1, 2, 3, 4"
-        expectedOutput = "steak, potato, wine, cake"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_night3421_then_output(self):
-        # arrange
-        input = "night, 3, 4, 2, 1"
-        expectedOutput = "steak, potato, wine, cake"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-    
-    def test_when_input_night1224_then_output(self):
-        # arrange
-        input = "night, 1, 2, 2, 4"
-        expectedOutput = "steak, potato(x2), cake"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-    
-    def test_when_input_night1235_then_output(self):
-        # arrange
-        input = "night, 1, 2, 3, 5"
-        expectedOutput = "steak, potato, wine, error"
-        currentOutput = ""
-        app = RestaurantApp()
-        # act
-        currentOutput = app.process_order(input)
-        # assert
-        assert currentOutput == expectedOutput
-        
-    def test_when_input_night1123_then_output(self):
-        # arrange
-        input = "night, 1, 1, 2, 3"
-        expectedOutput = "steak, error"
-        currentOutput = ""
+      
+    @pytest.mark.parametrize("input,expectedOutput", 
+    [
+        ("", "error"), 
+        (",", "error"), 
+        ("morning", ""), 
+        ("MORNING", ""), 
+        ("night", ""), 
+        ("NIGHT", ""), 
+        ("afternoon", "error"), 
+        ("morning, 1, 2, 3", "eggs, toast, coffee"), 
+        ("morning, 2, 1, 3", "eggs, toast, coffee"), 
+        ("morning, 1, 2, 3, 4", "eggs, toast, coffee, error"), 
+        ("morning, 4, 3, 2, 1", "eggs, toast, coffee, error"), 
+        ("morning, 3, 2, 1", "eggs, toast, coffee"), 
+        ("morning, 2, 1", "eggs, toast"), 
+        ("morning, 1", "eggs"), 
+        ("morning, 3, 2, 1", "eggs, toast, coffee"), 
+        ("morning, 1, 2, 3, 4", "eggs, toast, coffee, error"), 
+        ("morning, 1, 2, 3, 3, 3", "eggs, toast, coffee(x3)"),
+        ("MORNING  ,  1   ,  2,  3", "eggs, toast, coffee"), 
+        ("night, 1, 2, 3, 4", "steak, potato, wine, cake"), 
+        ("night, 3, 4, 2, 1", "steak, potato, wine, cake"), 
+        ("night, 1, 2, 2, 4", "steak, potato(x2), cake"), 
+        ("night, 1, 2, 3, 5", "steak, potato, wine, error"), 
+        ("night, 1, 1, 2, 3", "steak, error"),        
+        ("night, 4, 3, 2, 1", "steak, potato, wine, cake"), 
+        ("night, 3, 2, 1", "steak, potato, wine"), 
+        ("night, 2, 1", "steak, potato"), 
+        ("night, 1", "steak"),
+    ])
+    def test_when_input_then_validate_output(self, input:str, expectedOutput:str):
+        # arrange                
         app = RestaurantApp()
         # act
         currentOutput = app.process_order(input)
