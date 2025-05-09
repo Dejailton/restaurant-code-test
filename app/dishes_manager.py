38fb80c81f54556adb3632888206148dd9f51fda
Dejailton <devdejailton@gmail.com>
2025-05-08 23:18:30 -0300

    final test morning formatter

--- app/dishes_manager.py
+++ app/dishes_manager.py
@@ -1,6 +1,7 @@
 from .class_day.morning import Morning
+from .util.formatter import Formatter
 
-class DishesManager(Morning):
+class DishesManager(Formatter):
     def __init__(self):
         self.dishes = {}
     
@@ -10,16 +11,8 @@ class DishesManager(Morning):
                     return "error"
                 case "morning":
                     return ""
+                
     def process_order(self, input: str):
+        input = self.format_lower(input)
         output = self.manager_output(input)
         return output
-        
-        
-   
-        
-        
-
-        
-        
-        
-    
---
+++ app/util/formatter.py
@@ -0,0 +1,3 @@
+class Formatter:
+    def format_lower(self, input: str) -> str:
+        return input.lower()
