d838d24402536b1edd2b2cfd4bc9cc3c0cf51f31
Dejailton <devdejailton@gmail.com>
2025-05-11 20:09:02 -0300

    complete input morning12333

--- app/dishes_manager.py
+++ app/dishes_manager.py
@@ -1,5 +1,5 @@
 from typing import List
-from app.class_day.night import Night
+from .class_day.night import Night
 from .class_day.morning import Morning
 from .util.formatter import Formatter
 
@@ -7,17 +7,27 @@ class DishesManager(Formatter):
     def __init__(self):
         self.dishes = []
         
-    def verify_period(self, input: str):
-        if input[0] == "morning":
+    def verify_period(self, period: str):
+        if period == "morning":
             return Morning()
-        elif input[0] == "night":
+        elif period == "night":
             return Night()
     def menu_selection(self, input: List[str]) -> List[str]:
-        day = self.verify_period(input)
+        period = input[0]
+        day = self.verify_period(period)
+        count = 0
+        day_class = day
         day = day.get_dishes()
         input = self.list_order(input)
         for dish in input:
             dishe = day[int(dish)-1]
+            if period == "morning" and dish == "3":
+                count += 1
+                if count > 1:
+                    repeat = day_class.dishe_sum_autorized(input, period, day_class)
+                    self.dishes.clear()
+                    self.dishes.extend(repeat)
+                    break
             self.dishes.append(dishe)
     def manager_output(self, input:str) -> str:
         if len(input) == 1:
--- app/manager_day/manager.py
+++ app/manager_day/manager.py
@@ -8,3 +8,26 @@ class DayManager:
     
     def get_dishes(self):
         return self._dishes
+    
+    def dishe_sum_autorized(self, input, period, day: object):
+        count = 0
+        dishes = []
+        repeat = ""
+        if period == "morning":
+            for i in input:
+                if i == "3":
+                    count += 1
+                    if count >= 2:
+                        if f"coffee(x{count - 1})" in dishes:
+                            dishes.remove(f"coffee(x{count - 1})")
+                        repeat = f"coffee(x{count})"
+                        dishes.append(repeat)
+                dishes.append(day.get_dishes()[int(i)-1])
+                while "coffee" in dishes:
+                    dishes.remove("coffee")
+        elif period == "night":
+            for i in input:
+                if i == "2":
+                    count += 1
+                    repeat = f"potato(x{count})"
+        return dishes
